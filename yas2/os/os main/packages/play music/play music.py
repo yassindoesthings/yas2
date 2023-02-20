32677b250f0d320cc88378b847edcbbcbf058279
yassin.does.things <125836858+yassindoesthings@users.noreply.github.com>
2023-02-19 16:24:52 -0800

    Update play music.py

--- yas2/os/os main/packages/play music/play music.py
+++ yas2/os/os main/packages/play music/play music.py
@@ -5,7 +5,7 @@ songName = input("What is the name of the song you want to play? ")
 songArtist = input("Who is the song's artist? ")
 
 
-PATH = "C:/Users/defaultuser0/Downloads/yas2/os/os main/packages/play music/chromedriver.exe"
+PATH = ""
 driver = webdriver.Chrome(PATH)
 
 driver.get("https://www.youtube.com/results?search_query=" + songName + "+" + '"' + songArtist + '"' + '"topic"')
